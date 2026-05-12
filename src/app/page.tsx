94aafee4a07869571ce4171d17147c82b7258fa5
Facundo Fritz <facundofritz@gmail.com>
2026-05-12 14:54:45 -0300

    feat: mount Timeline section on landing page below Stats

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Hero from '@/components/Hero'
 import Stats from '@/components/Stats'
+import Timeline from '@/components/Timeline'
 
 export default function Home() {
   return (
     <main>
       <Hero />
       <Stats />
+      <Timeline />
     </main>
   )
 }
